7bbd33b7f44a94d6e74a1769f68ba95453e23d0a
Simon Goumaz <simon@attentif.ch>
2022-04-04 20:44:43 +0200

    Removed "main" from package.jon (it's not a lib)

--- src/main.js
+++ src/main.js
@@ -1,34 +1,34 @@
 /**
  * @license
  * Copyright (c) 2020-2021 Pryv S.A https://pryv.com
- * 
+ *
  * This file is part of Open-Pryv.io and released under BSD-Clause-3 License
- * 
- * Redistribution and use in source and binary forms, with or without 
+ *
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice, 
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
- * 3. Neither the name of the copyright holder nor the names of its contributors 
- *    may be used to endorse or promote products derived from this software 
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 const fs = require('fs');
@@ -37,7 +37,7 @@ const path = require('path');
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 const config = require('./load-config');
 
-const applyTemplate = require('./templating');
+const applyTemplate = require('./replaceVariables');
 
 
 let ignores, fileSpecs, specKeys, license;
@@ -96,7 +96,7 @@ function checkInit() {
 
 /**
  * Helper to find the corresponding specs for a file
- * @param {String} fullPath 
+ * @param {String} fullPath
  */
 function getFileSpec(fullPath) {
   for (const specKey of specKeys) {
@@ -133,9 +133,9 @@ async function handleMatchingFile(fullPath, spec) {
     }
   }
 
-  if (updatedFile) { 
+  if (updatedFile) {
     updated++;
-    
+
   }
   count++;
 }
@@ -145,7 +145,7 @@ async function handleMatchingFile(fullPath, spec) {
  * Loop recursively in the directory
  * - ignore files or dir matching one of the ignore items
  * - call handleMatchingFile each time a file matching a fileSpec is found
- * @param {String} dir 
+ * @param {String} dir
  */
 async function loop(dir) {
   //console.log('>' + dir);
@@ -177,4 +177,3 @@ let updated = 0;
   await start();
   console.log('Check license on ' + count + ' files, updated ' + updated + ' files in ' + Math.round((Date.now() - startTime) / 10) / 100 + ' s');
 })();
-
